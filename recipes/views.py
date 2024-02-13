25d474dec775e25069241034cd3d8ff24a110aa4
abdulgh19 <momademha@gmail.com>
2024-02-13 21:43:28 +0200

    Criando templates

--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('HOME 2')
+    return render(request, 'global/home.html')
 
 
 def contato(request):
